6166359699744ae2090f4850ebfc4190b3de915a
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-01-18 10:07:39 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/felipegabriel/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipegabriel.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
