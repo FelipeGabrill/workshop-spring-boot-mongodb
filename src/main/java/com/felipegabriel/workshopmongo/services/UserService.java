a1e4c229a3f83b69975626bf38f58f2f77565fda
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-01-19 09:30:27 -0300

    Atualização de usuário com PUT

--- src/main/java/com/felipegabriel/workshopmongo/services/UserService.java
+++ src/main/java/com/felipegabriel/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.felipegabriel.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,29 +15,44 @@ import com.felipegabriel.workshopmongo.services.exception.ObjectNotFoundExceptio
 @Service
 public class UserService {
 
-	
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
-	public User fromDTO (UserDTO objDto) {
+
+	public User update(User obj) {
+		try {
+			Optional<User> newObj = repo.findById(obj.getId());
+			User user = newObj.get();
+			updateData(user, obj);
+			return repo.save(user);
+		} catch (NoSuchElementException e) {
+		throw new ObjectNotFoundException("Object not found");
+		}
+	}
+
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
